8f054bcf1fb78625f801b946ae74dc6554bc4d73
unknown <Admin@AXLAP169.axcend.com>
2026-01-23 11:23:12 +0530

    Auto push from Ignition

---
+++ git_listener.ps1
@@ -0,0 +1,12 @@
+﻿$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:8090/gitpush/")
+$listener.Start()
+
+Write-Host "Listening on http://localhost:8090/gitpush/"
+
+while ($true) {
+    $context = $listener.GetContext()
+    Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -File C:\Users\Admin\Documents\IgnitionProject\git_push.ps1"
+    $context.Response.StatusCode = 200
+    $context.Response.Close()
+}
