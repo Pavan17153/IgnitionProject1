781965863afdc935fe54c17c482cb1a84c2814f0
unknown <Admin@AXLAP169.axcend.com>
2026-01-23 12:17:37 +0530

    Auto push from Ignition

---
+++ sync_and_push.ps1
@@ -0,0 +1,12 @@
+﻿# Step 1: Define Designer folder (source) and Git folder (destination)
+$source = "C:\Program Files\Inductive Automation\Ignition\data\projects\MyProject"
+$dest = "C:\Users\Admin\Documents\IgnitionProject"
+
+# Step 2: Copy updated files to Git folder
+Copy-Item $source\* $dest -Recurse -Force
+
+# Step 3: Go to Git folder and push to GitHub
+cd $dest
+git add .
+git commit -m "Auto push from Ignition"
+git push
